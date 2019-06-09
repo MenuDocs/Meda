f4ef8684b73c5d97040856ca1daace7e1a669abf
LolWastedJS <ml2dofficial@gmail.com>
2019-06-09 14:28:34 -0700

    Fixed the Ping Command

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -21,6 +21,6 @@ module.exports = {
     run: async (client, message, args) => {
         let random = ["Is this really my ping?", "Is it bad? I cant look.", "Dont tell me... its probably bad."][Math.floor(Math.random() * 3)]
         let msg = await message.send(`${random} : **Bot: \`${Math.round(client.ws.ping)}ms\`**`);
-        msg.edit(`${random} : **Bot: \`${Math.round(client.ws.ping)}ms\` API: \`${Math.round(msg.createdTimestamp-message.createdTimestamp)}ms\`**`)
+        msg.edit(client.embed.message(`${random} : **Bot: \`${Math.round(client.ws.ping)}ms\` API: \`${Math.round(msg.createdTimestamp-message.createdTimestamp)}ms\`**`));
     }
 }
